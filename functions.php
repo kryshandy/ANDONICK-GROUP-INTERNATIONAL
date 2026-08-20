382f98fcac558e3c0490f21e8e80b1029b261587
Tchuente Krys <tchuentekryshandy@gmail.com>
2026-08-20 09:47:24 +0100

    v3.8.0 - harmonie responsive sur tous ecrans : grilles metiers/temoignages/impacts 3->2->1 colonnes (tablette et mobile, jamais ecrasees), ancres non masquees sous le menu fixe, boutons WhatsApp contact empiles pleine largeur mobile, typographie fluide, rythme d'espacement homogene. Verifie a 1400/1024/768/480/380px : aucun debordement horizontal, boutons 448px pleine largeur

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ANDONICK_VERSION', '3.7.0' );
+define( 'ANDONICK_VERSION', '3.8.0' );
 define( 'ANDONICK_DIR', get_template_directory() );
 define( 'ANDONICK_URI', get_template_directory_uri() );
 
